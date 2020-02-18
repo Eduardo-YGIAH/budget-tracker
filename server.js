d708570dbe9196186124b1788587a1fe88e809b0
Eduardo Neto <edu.neto@icloud.com>
2020-02-18 15:50:26 +0000

    removed http redirects

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ const app = express();
 
 app.use(logger('dev'));
 
-app.use(function(req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
